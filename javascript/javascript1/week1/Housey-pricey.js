1a3c5279d313a563f8c978521c1649aa88a0b0d6
Elnaz-Talebi <nel.nazi1987@gmail.com>
2025-02-04 23:18:41 +0100

    Implemented task 3-Housey

---
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -0,0 +1,16 @@
+let peterHousePrice = 8 * 10 * 10 * 2.5 * 1000 + 100 * 300;
+let juliaHousePrice = 5 * 11 * 8 * 2.5 * 1000 + 70 * 300;
+let peterHouseCost = 2500000;
+let juliaHouseCost = 1000000;
+
+if (peterHouseCost > peterHousePrice) {
+    console.log("Peter is paying more than the house is really cost.");
+} else {
+    console.log("Peter is paying less or equal than house is actually cost.");
+}
+
+if (juliaHouseCost > juliaHousePrice) {
+    console.log("Julia is paying more than the house is really cost.");
+} else {
+    console.log("Julia is paying less or equal than house is actually cost.");
+}
