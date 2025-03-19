8ac5e97a0299a0cf30606d0eb1a7f4980adf126f
Elnaz-Talebi <nel.nazi1987@gmail.com>
2025-03-19 16:52:58 +0100

    Apply comments and redesign

--- javascript/javascript2/week2/index.js
+++ javascript/javascript2/week2/index.js
@@ -1,132 +1,74 @@
-const recipeList = [
-  {
-    id: 1,
-    title: "Gløgg",
-    picture_url:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Gl%C3%B6gg_kastrull.JPG/800px-Gl%C3%B6gg_kastrull.JPG",
-    ingredients: [
-      { NAME: "Orange zest", AMOUNT: "0.5" },
-      { NAME: "Water", AMOUNT: "200 ml" },
-      { NAME: "Sugar", AMOUNT: "275 g" },
-      { NAME: "Whole cloves", AMOUNT: "5" },
-      { NAME: "Cinnamon sticks", AMOUNT: "2" },
-      { NAME: "Spice", AMOUNT: undefined },
-      { NAME: "Bottle of red wine", AMOUNT: "1" },
-      { NAME: "Raisins", AMOUNT: "100 g" },
-      { NAME: "Slipped Almonds", AMOUNT: "50 g" },
-    ],
-    description: "Mix everything, heat it, and you are good to go!",
-  },
-];
+const storageKey = "data";
+const recipeList = loadFromStorage() ?? generateData();
+const activeTypeFilter = [];
 
 let searchList = [...recipeList];
 
-let ingredientCount = 5;
+function renderRecipe() {
+  const recipeSection = document.getElementById("recipe");
+  recipeSection.replaceChildren();
 
-let insertButton = document.getElementById("button-insert");
-insertButton.addEventListener("click", function () {
-  let insertForm = document.getElementById("recipe-form");
-  insertForm.style.display = "block";
-});
-
-function renderRecipe(recipeList) {
-  let secRecipe = document.getElementById("recipe");
-  secRecipe.replaceChildren();
-
-  recipeList.forEach((item) => {
-    let parent = document.createElement("div");
+  searchList.forEach((item) => {
+    const parent = document.createElement("div");
     parent.classList.add("every-recipe");
     parent.addEventListener("click", () => {
       showModal(item);
     });
 
-    let h3 = document.createElement("h3");
-    h3.innerHTML = item.title;
+    const h3 = document.createElement("h3");
+    h3.textContent = item.title;
     parent.appendChild(h3);
 
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = item.picture_url;
     parent.appendChild(img);
 
-    secRecipe.appendChild(parent);
+    const i = document.createElement("i");
+    i.classList.add("fa-solid");
+    i.classList.add("fa-eye");
+    parent.appendChild(i);
+
+    recipeSection.appendChild(parent);
   });
 }
+
 function search() {
   const keyword = document.getElementById("text-box").value;
   if (keyword) {
     searchList = recipeList.filter((item) => {
-      return item.title.toLowerCase() === keyword.toLowerCase();
+      return item.title.toLowerCase().includes(keyword);
     });
   } else {
     searchList = [...recipeList];
   }
-  searchList = sort(searchList);
-  renderRecipe(searchList);
+  resetSort();
+  renderRecipe();
 }
-
-let sortButton = document.getElementById("button-sort");
-sortButton.addEventListener("click", function () {
-  const recipeElement = document.getElementById("recipe");
-  const sort = recipeElement.dataset.sort;
-  switch (sort) {
-    case "none":
-      this.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
-      recipeElement.dataset.sort = "asc";
-      break;
-    case "asc":
-      this.innerHTML = '<i class="fa-solid fa-arrow-down"></i>';
-      recipeElement.dataset.sort = "desc";
-      break;
-    case "desc":
-      this.innerHTML = '<i class="fa-solid fa-arrows-up-down"></i>';
-      recipeElement.dataset.sort = "none";
-      break;
-  }
-  search();
-});
-
-function sort(list) {
-  const recipeElement = document.getElementById("recipe");
-  const sort = recipeElement.dataset.sort;
-  if (sort === "asc") {
-    return list.sort((a, b) => a.ingredients.length - b.ingredients.length);
-  }
-  if (sort === "desc") {
-    return list.sort((a, b) => b.ingredients.length - a.ingredients.length);
-  }
-  return list;
-}
-
-let submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  let form = document.getElementById("recipe-form");
-  const data = Object.fromEntries(new FormData(form));
-  const recipe = convertFormToObject(data);
-  recipeList.push(recipe);
-  let insertForm = document.getElementById("recipe-form");
-  insertForm.style.display = "none";
-  search();
-});
 
 function convertFormToObject(formData) {
+  const formDataArray = Array.from(formData.entries());
+  const formDataObject = Object.fromEntries(formDataArray);
+
+  const ingredientCount = formDataArray.filter(([key]) =>
+    key.startsWith("ingredients")
+  ).length;
+
   const result = {
     id: recipeList.length + 1,
-    title: formData.title,
-    picture_url: formData["picture-url"],
+    title: formDataObject["title"],
+    picture_url: formDataObject["picture-url"],
     ingredients: [],
-    description: formData.description,
+    description: formDataObject["description"],
   };
 
   for (let i = 0; i < ingredientCount; i++) {
-    const object = {
-      NAME: formData[`ingredients${i}`],
-      AMOUNT: formData[`amount${i}`],
+    const ingredientsObject = {
+      NAME: formDataObject[`ingredients${i}`],
+      AMOUNT: formDataObject[`amount${i}`],
     };
-    if (object.NAME){
-      result.ingredients.push(object);
+    if (ingredientsObject.NAME) {
+      result.ingredients.push(ingredientsObject);
     }
-    
   }
 
   return result;
@@ -136,41 +78,293 @@ function showModal(item) {
   const modalDetails = document.getElementById("modal-details");
   modalDetails.replaceChildren();
 
-  let h3 = document.createElement("h3");
-  h3.innerHTML = item.title;
+  const h3 = document.createElement("h3");
+  h3.textContent = item.title;
   modalDetails.appendChild(h3);
 
-  let img = document.createElement("img");
+  const img = document.createElement("img");
   img.src = item.picture_url;
+  img.alt = `${item.title} image`;
   modalDetails.appendChild(img);
 
-  let p = document.createElement("p");
-  p.innerHTML = "<p class='bold'>Description: </P>" + item.description;
-  modalDetails.appendChild(p);
+  const descriptionDiv = document.createElement("div");
+  descriptionDiv.innerHTML = `<p class='bold'>Description: </p><p> ${item.description} </p>`;
+  modalDetails.appendChild(descriptionDiv);
 
-  let ingDiv = document.createElement("div");
-  let ingP = document.createElement("p");
-  ingP.innerHTML = "Ingredients: ";
-  ingP.classList.add("bold");
-  ingDiv.appendChild(ingP);
+  const ingredientDiv = document.createElement("div");
+  const ingredientP = document.createElement("p");
+  ingredientP.textContent = "Ingredients: ";
+  ingredientP.classList.add("bold");
+  ingredientDiv.appendChild(ingredientP);
   item.ingredients.forEach((value) => {
     if (value?.NAME) {
-      let p = document.createElement("p");
-      p.innerHTML = `${value.NAME}: ${value.AMOUNT}`;
-      ingDiv.appendChild(p);
+      const p = document.createElement("p");
+      p.textContent = `${value.NAME}: ${value.AMOUNT}`;
+      ingredientDiv.appendChild(p);
     }
   });
 
-  modalDetails.appendChild(ingDiv);
+  modalDetails.appendChild(ingredientDiv);
 
   const modal = document.getElementById("recipe-modal");
   modal.style.display = "block";
-  console.log(item);
 }
 
-function hideModal() {
-  const modal = document.getElementById("recipe-modal");
-  modal.style.display = "none";
+function saveToStorage() {
+  localStorage.setItem(storageKey, JSON.stringify(recipeList, true));
 }
 
-renderRecipe(searchList);
+function loadFromStorage() {
+  const stringData = localStorage.getItem(storageKey);
+  if (stringData) {
+    return JSON.parse(stringData);
+  } else {
+    return null;
+  }
+}
+
+function renderTypes() {
+  const types = recipeList.map((item) => item.type);
+  const uniqTypes = types.filter((item, index) => {
+    return types.indexOf(item) === index;
+  });
+  const container = document.getElementById("type-filter-container");
+  uniqTypes.forEach((item) => {
+    const button = document.createElement("button");
+    button.innerHTML = item;
+    button.classList.add("btn_recipe");
+
+    button.addEventListener("click", () => {
+      const findIndex = activeTypeFilter.indexOf(item);
+      if (findIndex > -1) {
+        activeTypeFilter.splice(findIndex, 1);
+        button.classList.remove("btn_selected");
+      } else {
+        activeTypeFilter.push(item);
+        button.classList.add("btn_selected");
+      }
+    });
+    container.appendChild(button);
+  });
+}
+
+function tabSearch() {} // to be written
+
+function generateData() {
+  return [
+    {
+      id: 1,
+      title: "Gløgg",
+      type: "Alcoholic Drinks",
+      picture_url:
+        "https://images.unsplash.com/photo-1602522785856-8fc84c0f000d?q=80&w=2784&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "Orange zest", AMOUNT: "0.5", UNIT: "piece" },
+        { NAME: "Water", AMOUNT: "200", UNIT: "ml" },
+        { NAME: "Sugar", AMOUNT: "275", UNIT: "g" },
+        { NAME: "Whole cloves", AMOUNT: "5", UNIT: "pieces" },
+        { NAME: "Cinnamon sticks", AMOUNT: "2", UNIT: "pieces" },
+        { NAME: "Spice", AMOUNT: "1", UNIT: "pinch" },
+        { NAME: "Bottle of red wine", AMOUNT: "1", UNIT: "bottle" },
+        { NAME: "Raisins", AMOUNT: "100", UNIT: "g" },
+        { NAME: "Slipped Almonds", AMOUNT: "50", UNIT: "g" },
+      ],
+      description: "Mix everything, heat it, and you are good to go!",
+    },
+    {
+      id: 2,
+      title: "Pancakes",
+      type: "Vegetarian",
+      picture_url:
+        "https://images.unsplash.com/photo-1554520735-0a6b8b6ce8b7?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "All-purpose flour", AMOUNT: "200", UNIT: "g" },
+        { NAME: "Milk", AMOUNT: "250", UNIT: "ml" },
+        { NAME: "Eggs", AMOUNT: "2", UNIT: "pieces" },
+        { NAME: "Sugar", AMOUNT: "50", UNIT: "g" },
+        { NAME: "Baking powder", AMOUNT: "1", UNIT: "tsp" },
+        { NAME: "Butter", AMOUNT: "30", UNIT: "g" },
+        { NAME: "Salt", AMOUNT: "1", UNIT: "pinch" },
+      ],
+      description: "Mix all ingredients, cook in a pan until golden brown.",
+    },
+    {
+      id: 3,
+      title: "Tomato Soup",
+      type: "Vegetarian",
+      picture_url:
+        "https://images.unsplash.com/photo-1620791144170-8a443bf37a33?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "Tomatoes", AMOUNT: "500", UNIT: "g" },
+        { NAME: "Onion", AMOUNT: "1", UNIT: "piece" },
+        { NAME: "Garlic", AMOUNT: "2", UNIT: "cloves" },
+        { NAME: "Vegetable broth", AMOUNT: "500", UNIT: "ml" },
+        { NAME: "Olive oil", AMOUNT: "2", UNIT: "tbsp" },
+        { NAME: "Salt", AMOUNT: "1", UNIT: "tsp" },
+        { NAME: "Black pepper", AMOUNT: "0.5", UNIT: "tsp" },
+      ],
+      description: "Blend everything together and simmer for 20 minutes.",
+    },
+    {
+      id: 4,
+      title: "Guacamole",
+      type: "Vegan",
+      picture_url:
+        "https://plus.unsplash.com/premium_photo-1661741106657-cd8c21acec59?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "Avocados", AMOUNT: "2", UNIT: "pieces" },
+        { NAME: "Lime", AMOUNT: "1", UNIT: "piece" },
+        { NAME: "Tomato", AMOUNT: "1", UNIT: "piece" },
+        { NAME: "Onion", AMOUNT: "1", UNIT: "piece" },
+        { NAME: "Cilantro", AMOUNT: "10", UNIT: "g" },
+        { NAME: "Salt", AMOUNT: "1", UNIT: "tsp" },
+        { NAME: "Jalapeño", AMOUNT: "1", UNIT: "piece" },
+      ],
+      description: "Mash avocados and mix with the other ingredients.",
+    },
+    {
+      id: 5,
+      title: "Spaghetti Carbonara",
+      type: "Meat-Based",
+      picture_url:
+        "https://images.unsplash.com/photo-1588013273468-315fd88ea34c?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "Spaghetti", AMOUNT: "200", UNIT: "g" },
+        { NAME: "Egg yolks", AMOUNT: "3", UNIT: "pieces" },
+        { NAME: "Parmesan cheese", AMOUNT: "50", UNIT: "g" },
+        { NAME: "Pancetta", AMOUNT: "100", UNIT: "g" },
+        { NAME: "Black pepper", AMOUNT: "1", UNIT: "tsp" },
+        { NAME: "Salt", AMOUNT: "1", UNIT: "tsp" },
+      ],
+      description:
+        "Cook pasta, mix with eggs, cheese, pancetta, and season with pepper.",
+    },
+    {
+      id: 6,
+      title: "Chicken Curry",
+      type: "Meat-Based",
+      picture_url:
+        "https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      ingredients: [
+        { NAME: "Chicken breast", AMOUNT: "500", UNIT: "g" },
+        { NAME: "Onion", AMOUNT: "1", UNIT: "piece" },
+        { NAME: "Garlic", AMOUNT: "3", UNIT: "cloves" },
+        { NAME: "Coconut milk", AMOUNT: "400", UNIT: "ml" },
+        { NAME: "Curry powder", AMOUNT: "2", UNIT: "tbsp" },
+        { NAME: "Salt", AMOUNT: "1", UNIT: "tsp" },
+        { NAME: "Olive oil", AMOUNT: "1", UNIT: "tbsp" },
+      ],
+      description:
+        "Sauté ingredients, add coconut milk, and simmer for 20 min.",
+    },
+  ];
+}
+
+function resetSort() {
+  const recipeElement = document.getElementById("recipe");
+  recipeElement.dataset.sort = "none";
+  const buttonSort = document.getElementById("button-sort");
+  buttonSort.innerHTML = '<i class="fa-solid fa-arrows-up-down"></i>';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const insertRecipeButton = document.getElementById("button-insert");
+  insertRecipeButton.addEventListener("click", function () {
+    const insertRecipeForm = document.getElementById("recipe-form");
+    insertRecipeForm.style.display = "block";
+  });
+
+  const sortButton = document.getElementById("button-sort");
+  sortButton.addEventListener("click", function () {
+    const recipeElement = document.getElementById("recipe");
+    const sort = recipeElement.dataset.sort;
+    switch (sort) {
+      case "none":
+        this.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
+        recipeElement.dataset.sort = "asc";
+        searchList = searchList.sort(
+          (a, b) => a.ingredients.length - b.ingredients.length
+        );
+        break;
+      case "asc":
+        this.innerHTML = '<i class="fa-solid fa-arrow-down"></i>';
+        recipeElement.dataset.sort = "desc";
+        searchList = searchList.sort(
+          (a, b) => b.ingredients.length - a.ingredients.length
+        );
+        break;
+      case "desc":
+        this.innerHTML = '<i class="fa-solid fa-arrows-up-down"></i>';
+        recipeElement.dataset.sort = "none";
+        searchList = searchList.sort((a, b) => a.id - b.id);
+        break;
+    }
+    renderRecipe();
+  });
+
+  const submitButton = document.getElementById("submit");
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const form = document.getElementById("recipe-form");
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+
+    const data = new FormData(form);
+
+    const recipe = convertFormToObject(data);
+    recipeList.push(recipe);
+    saveToStorage();
+    form.reset();
+    form.style.display = "none";
+    search();
+  });
+
+  const closeModalButton = document.getElementById("recipe-modal-close");
+  closeModalButton.addEventListener("click", function () {
+    const modal = document.getElementById("recipe-modal");
+    modal.style.display = "none";
+  });
+
+  const closeFormButton = document.getElementById("recipe-form-close");
+  closeFormButton.addEventListener("click", function () {
+    const form = document.getElementById("recipe-form");
+    form.style.display = "none";
+  });
+
+  const ingredientAddButton = document.getElementById("button-add-ingredient"); // to be written
+  ingredientAddButton.addEventListener("click", function () {
+    const index = document.getElementsByClassName("ingredients-item").length;
+
+    const divItem = document.createElement("div");
+    divItem.classList.add("ingredients-item");
+    divItem.classList.add("grid");
+    const inputIngredient = document.createElement("input");
+    inputIngredient.setAttribute("placeholder", "Ingredients of your recipe");
+    inputIngredient.name = `ingredients${index}`;
+    divItem.appendChild(inputIngredient);
+    const inputAmount = document.createElement("input");
+    inputAmount.setAttribute("placeholder", "Amounts of your recipe");
+    inputAmount.name = `amount${index}`;
+    divItem.appendChild(inputAmount);
+    const inputUnit = document.createElement("select");
+    inputUnit.name = `unit${index}`;
+    inputUnit.innerHTML = `<option value="">pieces</option>
+              <option>g</option>
+              <option>kg</option>
+              <option>cloves</option>
+              <option>tbsp</option>
+              <option>tsp</option>
+              <option>l</option>
+              <option>ml</option>
+              <option>pinch</option>
+              <option>bottles</option>`;
+    divItem.appendChild(inputUnit);
+    const divContainer = document.getElementById("ingredients-container");
+    divContainer.appendChild(divItem);
+  });
+
+  renderTypes();
+  renderRecipe();
+});
